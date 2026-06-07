ce507022299f718bf010c55de717a76c1a9692b2
Hayvid <davidhyppolitedh@gmail.com>
2026-06-07 08:46:02 -0500

    Add ACR, AcrPull binding, and app namespace
    - Provision Azure Container Registry (Basic SKU, admin disabled)
    - Grant AcrPull on the AKS kubelet identity so pods can pull images
      without admin credentials
    - Create dedicated "app" Kubernetes namespace

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,4 +33,22 @@ resource "azurerm_kubernetes_cluster" "k8s" {
   }
 }
 
+resource "kubernetes_namespace_v1" "app" {
+  metadata {
+    name = "app"
+  }
+}
 
+resource "azurerm_container_registry" "acr" {
+  name                = "devplatformpoc"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = false
+}
+
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.k8s.kubelet_identity[0].object_id
+}
